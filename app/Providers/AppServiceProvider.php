9c8239af2b83d36756ec77ae64700c11d2d86274
xBigDaddyx <yussupcazzanova97@gmail.com>
2025-03-29 22:21:19 +0700

    removed debugging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(UserRepository::class, EloquentUserRepository::class);
+//        $this->app->bind(UserRepository::class, EloquentUserRepository::class);
     }
 
     /**
